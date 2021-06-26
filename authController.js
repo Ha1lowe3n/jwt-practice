f609892ab5c44ab3c14948c689dd68b27998a57d
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-06-27 07:49:09 +0800

    add registration, validation for request

--- authController.js
+++ authController.js
@@ -1,18 +1,34 @@
+const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
+
 const User = require("./models/User");
 const Role = require("./models/Role");
 
 class authController {
     async registration(req, res) {
         try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res
+                    .status(400)
+                    .json({ message: "Ошибка при регистрации", errors });
+            }
             const { username, password } = req.body;
-            const candidate = await User.findOne(username);
-            console.log(candidate);
+            const candidate = await User.findOne({ username });
             if (candidate) {
                 return res.status(400).json({
                     message: "Пользователь с таким именем уже существует",
                 });
             }
-            const user = new User({});
+            const hashPassword = bcrypt.hashSync(password, 7);
+            const userRole = await Role.findOne({ value: "USER" });
+            const user = new User({
+                username,
+                password: hashPassword,
+                roles: [userRole.value],
+            });
+            await user.save();
+            return res.json({ message: "Регистрация успешно завершена" });
         } catch (e) {
             console.error(e);
             res.status(400).json({ message: "Registration error" });
