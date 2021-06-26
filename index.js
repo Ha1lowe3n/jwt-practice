ba99920253e1dda6cabb9cee9f8a8834bb362bb9
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-06-26 20:23:52 +0800

    connect to mongodb

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+const start = async () => {
+    try {
+        await mongoose.connect(
+            "mongodb+srv://user:user@cluster0.vg1vg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+            { useNewUrlParser: true, useUnifiedTopology: true }
+        );
+        app.listen(5000, () => console.log("Server is working on post 5000 "));
+    } catch (e) {
+        console.error(e);
+    }
+};
+start();
